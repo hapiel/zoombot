b95e983a7ad17fc6cf242369b718f5f9ebae271d
Umbohr <umbohrilege@gmail.com>
2024-01-19 10:45:11 +0100

    verify if pixel art is inactive

--- commands/utility/random.js
+++ commands/utility/random.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const pixelJointUrl ="https://pixeljoint.com/pixels/new_icons.asp?ob=date";
 const pixelJointPixelartUrl ="https://pixeljoint.com/pixelart/";
+const pixelArtInactiveString = "ACHTUNG! PIXEL ART INACTIVE!";
 const minIndex = 1000;
 const https = require('https');
 
@@ -9,38 +10,48 @@ module.exports = {
 		.setName('random')
 		.setDescription('Replies with a random PJ Art piece'),
 	async execute(interaction) {
-    console.log("attends")
         // get last art piece index
-        https.request(pixelJointUrl, getRandomPjPiece.bind(this, function(data){
-            interaction.reply(data);
-        })).end();
+        const url = await fetch(pixelJointUrl).then(function(response) {
+          return response.text();
+        }).then(function(data){
+          return getRandomPjPiece(data);
+        });
+        interaction.reply(url);
 	},
     
 };
 
 // CallbackFunction that get last challenge url on pixeljoint.com
-getRandomPjPiece = function(callback, response) {
-    // get the data as a string instead of a buffer
-    response.setEncoding();
-    var store = "";
-    // on each update, add to the store
-    response.on('data', function(d) {
-        store += d;
-    }); 
-  
-    //when stream is done, do the thing
-    response.on('end', function() {
+getRandomPjPiece = async function(data) {
         const indexStartString = "<div class='imgbox' id='bx";
-        const startUrl = store.indexOf(indexStartString);
+        const startUrl = data.indexOf(indexStartString);
         const endUrl = startUrl + indexStartString.length + 6;
-        const index = store.substring(startUrl + indexStartString.length, endUrl -1);
+        const index = data.substring(startUrl + indexStartString.length, endUrl -1);
         const maxIndexNumber = Number(index);
         if(startUrl !== -1 && endUrl !== -1 && maxIndexNumber){
-          const index = getRandomArbitrary(minIndex, maxIndexNumber);
-          const randomUrl = pixelJointPixelartUrl + index + '.htm';
-          callback(randomUrl);
+          let index = getRandomArbitrary(minIndex, maxIndexNumber);
+          let randomUrl = pixelJointPixelartUrl + index + '.htm';
+          let isInactive = true;
+          while (isInactive) {
+            randomUrl = await fetch(randomUrl).then(function(response) {
+              return response.text();
+            }).then(function(data){
+              isInactive = data.includes(pixelArtInactiveString);
+              if( isInactive) {
+                console.log('Pixel art inactive, retrying')
+                index = getRandomArbitrary(minIndex, maxIndexNumber);
+              }
+              return pixelJointPixelartUrl + index + '.htm';
+            });
+          }
+          return randomUrl;
         }
-    });
+
+  }
+
+  function checkIfInactive(){
+
+
   }
 
   function getRandomArbitrary(min, max) {
