2ec8c309df631df2c5eec246f38e15d2931f371a
Umbohr <umbohrilege@gmail.com>
2025-09-02 21:01:44 +0200

    fix delay

--- common/imageUtils.js
+++ common/imageUtils.js
@@ -23,11 +23,7 @@ sendScaled = async function (msg, key, width, height, spoiler, isGif) {
             .gif()
             .toBuffer()
             .then(buffer => {
-<<<<<<< HEAD
               msg.editReply({ content: '', files: [{ attachment: buffer, name: "2X.gif" }] });
-=======
-              msg.reply({ content: '', files: [{ attachment: buffer, name: spoiler? "SPOILER_2X.gif":"2X.gif" }] });
->>>>>>> 2b14e707af4bdc5eaf51e5043560f043fc374628
             });
         }
         else {
@@ -36,11 +32,7 @@ sendScaled = async function (msg, key, width, height, spoiler, isGif) {
             .png()
             .toBuffer()
             .then(buffer => {
-<<<<<<< HEAD
               msg.editReply({ content: '', files: [{ attachment: buffer, name: "2X.png" }] });
-=======
-              msg.reply({ content: '', files: [{ attachment: buffer, name: spoiler? "SPOILER_2X.png":"2X.png" }] });
->>>>>>> 2b14e707af4bdc5eaf51e5043560f043fc374628
             });
         }
       })
